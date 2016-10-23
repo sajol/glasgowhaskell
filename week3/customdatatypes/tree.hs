c24c4aafd3f0fb9c0acc888c13889ce9124d70c6
sazal <sajolkk@gmail.com>
2016-10-23 23:48:53 +0600

    tree traversal

--- week3/customdatatypes/tree.hs
+++ week3/customdatatypes/tree.hs
@@ -30,7 +30,20 @@ addNewMax (Node value left right) = Node value left (addNewMax right)
 
 tree = (Node 2 (Node 1 Leaf Leaf) (Node 3 Leaf Leaf))
 
+traversePreOrder :: Tree -> [Int]
+traversePreOrder Leaf = []
+traversePreOrder (Node value left right) = [value] ++ traversePreOrder left ++ traversePreOrder right
+
+traverseInOrder :: Tree -> [Int]
+traverseInOrder Leaf = []
+traverseInOrder (Node value left right) = traverseInOrder left ++ [value] ++ traverseInOrder right
+
+traversePostOrder :: Tree -> [Int]
+traversePostOrder Leaf = []
+traversePostOrder (Node value left right) = traversePostOrder left ++ traversePostOrder right ++ [value]
+
+
 dfs :: Tree -> [Int]
 dfs Leaf = []
-dfs (Node value left right) = value : dfs left ++ dfs right
+dfs (Node value left right) = [value] ++ dfs left ++ dfs right
 
