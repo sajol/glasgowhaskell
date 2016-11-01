c8499ce6c12351e98be88ca60597d3d20110d184
sazal <sajolkk@gmail.com>
2016-11-02 00:07:13 +0600

    json library

---
+++ exercise/library/SimpleJSON.hs
@@ -0,0 +1,53 @@
+
+--a library to manipulate and serialize data in json form
+module SimpleJSON
+    (
+        JValue(..),
+        getString,
+        getInt,
+        getDouble,
+        getBool,
+        getArray,
+        getObject,
+        isNull
+    ) where
+
+
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject [(String, JValue)]
+            | JArray [JValue]
+              deriving (Eq, Ord, Show)
+
+getString :: JValue -> Maybe String
+getString (JString s) = Just s
+getString _ = Nothing
+
+getInt :: JValue -> Maybe Int
+getInt (JNumber n) = Just (truncate n)
+getInt _ = Nothing
+
+getDouble :: JValue -> Maybe Double
+getDouble (JNumber d) = Just d
+getDouble _ = Nothing
+
+getBool :: JValue -> Maybe Bool
+getBool (JBool b) = Just b
+getBool _ = Nothing
+
+getObject :: JValue -> Maybe [(String,JValue)]
+getObject (JObject a) = Just a
+getObject _ = Nothing
+
+
+getArray :: JValue -> Maybe [JValue]
+getArray (JArray a) = Just a
+getArray _ = Nothing
+
+
+isNull :: JValue -> Bool
+isNull v = v == JNull
+
+
