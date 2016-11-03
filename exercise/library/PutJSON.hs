1b82a8010dafc4f067b2652642a477ac5b821185
sajol <sajolkk@gmail.com>
2016-11-03 19:35:27 +0600

    print json data

---
+++ exercise/library/PutJSON.hs
@@ -0,0 +1,33 @@
+
+module PutJSON where
+import Data.List(intercalate)
+import SimpleJSON
+
+renderJValue :: JValue -> String
+
+renderJValue (JString s) = show s
+
+renderJValue (JNumber n) = show n
+
+renderJValue (JBool True) = "true"
+
+renderJValue (JBool False) = "false"
+
+renderJValue JNull = "null"
+
+renderJValue (JObject o) = "{" ++ pairs o ++ "}"
+    where pairs [] = ""
+          pairs ps = intercalate ", " (map renderPairs ps)
+          renderPairs (k,v) = show k ++ ":" ++ renderJValue v
+
+renderJValue (JArray a) = "[" ++ values a ++ "]"
+    where values [] = ""
+          values a = intercalate ", " (map renderJValue a)
+
+
+putJValue :: JValue -> IO()
+putJValue v = putStrLn(renderJValue v)
+
+
+
+
