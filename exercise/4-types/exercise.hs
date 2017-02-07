110ece7e47cd4a08854693df7073182e43c6d824
sajol <sajolkk@gmail.com>
2017-02-08 00:37:23 +0600

    exercise on types

---
+++ exercise/4-types/exercise.hs
@@ -0,0 +1,32 @@
+module Exeercise where
+
+a = (* 9) 6
+
+b = head [(0,"doge"),(1,"kitteh")]
+
+c = head [(0 :: Integer ,"doge"),(1,"kitteh")]
+
+d = if False then True else False
+
+e = length [1, 2, 3, 4, 5]
+
+f = (length [1, 2, 3, 4]) > (length "TACOCAT")
+
+
+x = 5
+y = x + 5
+w = y * 10
+
+z = 4 / y
+
+
+
+
+g = "Julie"
+h = " <3"
+i = "Haskell"
+j = g ++ h ++ i
+
+
+
+
