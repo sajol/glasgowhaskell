9edb43f2b94c7caf835b55bdd706b395dcbd7e4a
sajol <sajolkk@gmail.com>
2017-02-14 23:55:56 +0600

    exercise : composition

--- exercise/7-funictional-patterns/functionComposition.hs
+++ exercise/7-funictional-patterns/functionComposition.hs
@@ -38,3 +38,62 @@ main = do
     print (negate (addOne 0))
     print ((negate . addOne) 0)
     print ((addOne . addOne . addOne . negate . addOne) 0)
+
+
+myPrint :: (Show a) => a -> IO ()
+myPrint a = (putStrLn . show) a
+
+pointFreePrint :: (Show a) => a -> IO ()
+pointFreePrint = putStrLn . show
+
+
+--exercise : lets write code
+--1
+
+tensDigit :: Integral a => a -> a
+tensDigit x = d
+    where xLast = (fst . divMod x) 10
+          d = xLast `mod` 10
+
+--2
+foldBool :: a -> a -> Bool -> a
+foldBool x y z =
+    case z of
+        True -> x
+        False -> y
+
+--pattern matching version
+foldBool3 :: a -> a -> Bool -> a
+foldBool3 x y True = x
+foldBool3 x y False = y
+
+
+
+--3
+g :: (a -> b) -> (a, c) -> (b, c)
+g f (x, y) = (f x, y)
+
+
+--4
+roundTrip :: (Show a, Read a) => a -> a
+roundTrip a = read (show a)
+
+exercise4executor =
+    do
+        print (roundTrip 4)
+        print (id 5)
+
+
+--point free
+roundTrip' :: (Show a, Read a) => a -> a
+roundTrip' = read . show
+
+exercise4executor' =
+    do
+        (print . roundTrip') 4
+        (print . id) 5
+
+
+roundTrip'' :: (Show a, Read b) => a -> b
+roundTrip'' = read . show
+--ex : roundTrip'' 1 :: Integer
