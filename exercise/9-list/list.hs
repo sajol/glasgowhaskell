af56e725b677ea0894892950935ceb98086ae4fb
sajol <sajolkk@gmail.com>
2017-02-17 23:13:19 +0600

    list

--- exercise/9-list/list.hs
+++ exercise/9-list/list.hs
@@ -24,5 +24,19 @@ myEnumFromTo a b
     | otherwise = buildList a b []
     where
         buildList from to accumulator
-            | from > to = accumulator
-            | from <= to = buildList (succ from) to (accumulator ++ [from])
+            | from < to = buildList (succ from) to (accumulator ++ [from])
+            | otherwise = accumulator ++ [from]
+
+
+
+eftBool :: Bool -> Bool -> [Bool]
+eftBool a b = myEnumFromTo a b
+
+eftOrd :: Ordering -> Ordering -> [Ordering]
+eftOrd a b = myEnumFromTo a b
+
+eftInt :: Int -> Int -> [Int]
+eftInt a b = myEnumFromTo a b
+
+eftChar :: Char -> Char -> [Char]
+eftChar a b = myEnumFromTo a b
