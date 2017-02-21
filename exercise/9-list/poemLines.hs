77a57f1303d2c608b2718f81ca29d33d475a03f8
sajol <sajolkk@gmail.com>
2017-02-21 23:25:12 +0600

    list extraction : intermission exercise

---
+++ exercise/9-list/poemLines.hs
@@ -0,0 +1,45 @@
+module PoemLines where
+
+{-
+Next, write a function that takes a string and returns a list of
+strings, using newline separators to break up the string as in the
+following
+-}
+
+
+
+firstSen = "Tyger Tyger, burning bright\n"
+secondSen = "In the forests of the night\n"
+thirdSen = "What immortal hand or eye\n"
+fourthSen = "Could frame thy fearful symmetry?"
+sentences = firstSen ++ secondSen
+            ++ thirdSen ++ fourthSen
+
+
+
+
+myLines :: String -> [String]
+myLines s =
+    case takeWhile (/= '\n') dropInitialNewLine of
+        [] -> []
+        xs -> xs : myLines rest
+        where
+            dropInitialNewLine = dropWhile (== '\n') s
+            rest = dropWhile (/= '\n') dropInitialNewLine
+
+
+
+
+shouldEqual =
+  [ "Tyger Tyger, burning bright"
+  , "In the forests of the night"
+  , "What immortal hand or eye"
+  , "Could frame thy fearful symmetry?"
+  ]
+
+
+
+main :: IO ()
+main =
+    print $ "Are they equal? "
+        ++ show (myLines sentences == shouldEqual)
