18069e7f30745e7243850669f327c616d91ee380
sajol <sajolkk@gmail.com>
2017-02-28 00:20:08 +0600

    fold : exercise

--- exercise/10-fold/foldTest.hs
+++ exercise/10-fold/foldTest.hs
@@ -19,3 +19,13 @@ myProduct (x : xs) = x * product xs
 myConcat :: [[a]] -> [a]
 myConcat [] = []
 myConcat (x : xs) = x ++ myConcat xs
+
+
+myFoldr :: (a -> b -> b) -> b -> [a] -> b
+myFoldr f acc [] = acc
+myFoldr f acc (x : xs) = f x (myFoldr f acc xs)
+
+
+myFoldl :: (b -> a -> b) -> b -> [a] -> b
+myFoldl f acc [] = acc
+myFoldl f acc (x : xs) = myFoldl f (f acc x) xs
