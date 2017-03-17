43df71eade267982e810e98d6870c175a3ef123e
sajol <sajolkk@gmail.com>
2017-03-17 11:16:38 +0600

    what makes it algebraic (cardinality) : exercise

--- exercise/11-algebraic-data-types/cardinality.hs
+++ exercise/11-algebraic-data-types/cardinality.hs
@@ -28,3 +28,28 @@ data Airline = PapuAir
 -- It seems 256 = 2 ^ 8
 
 
+-- Simple datatypes with nullary data constructors
+
+data Example = MakeExample deriving Show
+
+--1. What is the type of data constructor MakeExample ?
+--What happens when you request the type of Example ?
+
+--type of MakeExample : Example
+--Not in scope: data constructor ‘Example’
+
+
+--2.What if you try :info on Example in GHCi?
+--Can you determine what typeclass instances are defined
+--for the Example type using :info in GHCi?
+
+-- info shows that it is instance of Show type class
+
+
+--3.Try making a new datatype like Example but with a single
+--type argument added to MakeExample , such as Int .
+-- What has changed when you query MakeExample with :type in GHCi?
+
+data Example1 = MakeExample1 Int deriving Show
+
+-- :t MakeExample1 will give Int -> Example
