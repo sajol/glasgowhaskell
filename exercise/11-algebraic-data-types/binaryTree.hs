0bd5c49eff175fdc9cd60a48ba1482a4cba1a05b
sajol <sajolkk@gmail.com>
2017-03-23 23:31:47 +0600

    write map for binary tree

--- exercise/11-algebraic-data-types/binaryTree.hs
+++ exercise/11-algebraic-data-types/binaryTree.hs
@@ -17,3 +17,23 @@ n1 = insert 0 Leaf
 n2 = insert 3 n1
 n3 = insert 5 n2
 n4 = insert 2 n3
+
+
+mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
+mapTree f Leaf = Leaf
+mapTree f (Node left a right) =
+    Node (mapTree f left) (f a) (mapTree f right)
+
+
+testTree :: BinaryTree Integer
+testTree = Node (Node Leaf 3 Leaf) 1 (Node Leaf 4 Leaf)
+
+
+mapExpected = Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)
+
+
+--acceptance test for mapTree
+mapOkay =
+    if mapTree (+1) testTree == mapExpected
+    then print "yup okay!"
+    else error "test failed"
